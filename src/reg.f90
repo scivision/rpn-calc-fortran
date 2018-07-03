5013c96fc8c76eb6adf169c7805f0b4c00f61d7a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-03 15:19:25 -0400

    version bump

--- src/reg.f90
+++ src/reg.f90
@@ -3,7 +3,7 @@ use assert, only: wp
 
 IMPLICIT NONE
 
-CHARACTER(*), PARAMETER :: VERSION = '1.1.4'
+CHARACTER(*), PARAMETER :: VERSION = '1.1.5'
 
 INTEGER :: STACK_SIZE                                         ! stack size
 INTEGER, PARAMETER :: REG_SIZE = 10                                           ! number of storage registers
