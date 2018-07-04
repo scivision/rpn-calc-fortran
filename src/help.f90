2cf4c3b1e0335d9fde40a8885366690a693055cb
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-03 20:20:26 -0400

    init

---
+++ src/help.f90
@@ -0,0 +1,27 @@
+module help
+use, intrinsic:: iso_fortran_env, only: stderr=>output_unit
+
+implicit none
+
+contains
+
+subroutine print_help()
+! Fortran doesn't know the location of its executable, making it interested to read a help file in a relative location.
+
+print *, achar(13),'See README.md for complete documentation'
+print *,'https://github.com/scivision/rpn-calc-fortran',new_line('')
+
+print *,'Mode calculator is in:   MODES'
+print *,'Stack:  R - roll down     U - roll up      D - drop'
+print *,'        CLALL - clear all     CLS - Clear stack'
+print *,'Number systems:     REAL     COMPLEX     RATIONAL'
+print *,'Angular systems:    DEG     RAD     GRAD'
+print *,'Number base:  BIN    DEC    HEX    OCT'
+print *,'Register memory:  STOx   RCLx   where x ~ 0..9'
+
+
+
+end subroutine print_help
+
+
+end module help
