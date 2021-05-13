f9c1d6b08613bbe3f0ef0fb8cb658b48756e1ded
Michael Hirsch <scivision@users.noreply.github.com>
2021-05-13 00:09:38 -0400

    cmake native stdin test

---
+++ cmake/test_stdin.cmake
@@ -0,0 +1,15 @@
+function(test_stdin tgt txt)
+
+execute_process(
+COMMAND ${tgt}
+INPUT_FILE ${txt}
+TIMEOUT 10
+RESULT_VARIABLE err)
+
+if(NOT err EQUAL 0)
+  message(FATAL_ERROR "${tgt} ${err}")
+endif()
+
+endfunction(test_stdin)
+
+test_stdin(${tgt} ${txt})
