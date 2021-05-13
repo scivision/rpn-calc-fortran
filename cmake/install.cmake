c9c7d96d6e84f30b5b69bf3eaecf2848c1fa1ce2
Michael Hirsch <scivision@users.noreply.github.com>
2021-05-13 00:17:23 -0400

    rpncalc install

---
+++ cmake/install.cmake
@@ -0,0 +1,44 @@
+# --- BOILERPLATE: install / packaging
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+  INSTALL_DESTINATION lib)
+
+write_basic_package_version_file(
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  COMPATIBILITY SameMinorVersion)
+
+install(EXPORT ${PROJECT_NAME}Targets
+  NAMESPACE ${PROJECT_NAME}::
+  DESTINATION lib/cmake/${PROJECT_NAME})
+
+install(FILES
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  DESTINATION lib/cmake/${PROJECT_NAME})
+
+# instead of export() which would need duplicates everywhere of install() calls,
+# just default install to build dir.
+
+# --- CPack
+
+set(CPACK_GENERATOR "TZST")
+set(CPACK_SOURCE_GENERATOR "TZST")
+set(CPACK_PACKAGE_VENDOR "Michael Hirsch")
+set(CPACK_PACKAGE_CONTACT "Michael Hirsch")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+set(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/package")
+set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+
+# not .gitignore as its regex syntax is more advanced than CMake
+file(READ ${CMAKE_CURRENT_LIST_DIR}/.cpack_ignore _cpack_ignore)
+string(REGEX REPLACE "\n" ";" _cpack_ignore ${_cpack_ignore})
+set(CPACK_SOURCE_IGNORE_FILES "${_cpack_ignore}")
+
+install(FILES ${CPACK_RESOURCE_FILE_README} ${CPACK_RESOURCE_FILE_LICENSE}
+  DESTINATION share/docs/rpncalc-fortran/${PROJECT_NAME})
+
+include(CPack)
