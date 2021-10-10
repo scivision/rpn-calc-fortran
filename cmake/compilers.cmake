0ade063f11a901f7eb380eadbc49dc2203a40b1c
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-10 11:38:04 -0400

    cmake best practices compiler flags

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -2,21 +2,12 @@ set(CMAKE_CONFIGURATION_TYPES "Release;Debug" CACHE STRING "Build type selection
 
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-mtune=native)
-
-  string(APPEND CMAKE_Fortran_FLAGS " -fimplicit-none")
-  #-Wall #-Wextra  -Warray-temporaries -Werror=array-bounds
-
-  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -fcheck=all -fexceptions -ffpe-trap=invalid,zero,overflow -finit-real=nan -Wconversion")
-
+  add_compile_options(-mtune=native -fimplicit-none -Werror=line-truncation
+  "$<$<CONFIG:Debug>:-fcheck=all;-fexceptions;-ffpe-trap=invalid,zero,overflow;-finit-real=nan;-Wconversion>"
+  )
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
-  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -warn -fpe0 -traceback")# -debug extended -check all)
-
-  if(WIN32)
-    add_compile_options(/QxHost)
-    string(APPEND CMAKE_Fortran_FLAGS " /warn:declarations")
-  else()
-    add_compile_options(-xHost)
-    string(APPEND CMAKE_Fortran_FLAGS " -warn declarations")
-  endif()
+  add_compile_options(
+  "$<$<CONFIG:Debug>:-warn;-fpe0;-traceback>"
+  "$<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>"
+  )
 endif()
